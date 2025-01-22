477d41e05b85ac4fa62e4a9a9a1c611d57b3c901
Semih Turan <semituran@gmail.com>
2025-01-22 11:38:24 +0300

    feat(posts): Add initial posts service implementation
    - Set up Express server for posts service
    - Create basic posts service structure
    - Add index.js as service entry point
    - Configure package.json for posts microservice
    
    This commit establishes the foundation for the posts
    microservice component.

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto"); // randomBytes is a function that generates a random string of bytes
+
+const app = express();
+app.use(bodyParser.json()); // bodyParser is a middleware that parses the body of the request
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+    const id = randomBytes(4).toString("hex");
+    const { title } = req.body;
+    posts[id] = {
+        id,
+        title
+    };
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+});
